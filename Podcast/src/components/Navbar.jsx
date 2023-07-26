5a4f3b77f5f6e81657ad91bc3e5eefa490065f72
Tshireletso46 <125552061+Tshireletso46@users.noreply.github.com>
2023-07-26 11:51:44 +0200

    update

--- Podcast/src/components/Navbar.jsx
+++ Podcast/src/components/Navbar.jsx
@@ -1,6 +1,17 @@
 export default function Navbar() {
-    <>
-    <h1>Podcast</h1>
-    </>
+    return(
+        <>
+        <nav>
+            <header>
+               <button> <img src="./images/Logo.png" alt="Logo" className="logo" /></button>
+            </header>
+            <li className="list">
+                <button>Favourites</button>
+                <button>search</button>
+            </li>
+        </nav>
+        </>
+    )
+   
 
 }
